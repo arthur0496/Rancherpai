868104efe0566093d5d3dad363e9abdc3db811c1
Arthur Assis <arthur120496@gmail.com>
2018-06-05 15:57:39 -0300

    Creates basic bot structure
    Co-authored-by: dedao <andre.filho001@outlook.com>
    Co-authored-by: alaques <alaxallves@gmail.com>

---
+++ rancherpai.py
@@ -0,0 +1,55 @@
+import os
+import time
+import re
+from slackclient import SlackClient
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+starterbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_bot_commands(slack_events):
+  for event in slack_events:
+    if event["type"] == "message" and not "subtype" in event:
+      user_id, message = parse_direct_mention(event["text"])
+      if user_id == starterbot_id:
+        return message, event["channel"]
+  return None, None
+
+def parse_direct_mention(message_text):
+   matches = re.search(MENTION_REGEX, message_text)
+   if matches : 
+    return (matches.group(1), matches.group(2).strip()) 
+   else:
+     return (None, None)
+
+def handle_command(command,channel):
+  default_response = "deu ruim!!!"
+  
+  response = None
+  if command.startswith(EXAMPLE_COMMAND):
+    response = "deu bom!!!"
+
+  slack_client.api_call(
+    "chat.postMessage",
+    channel=channel,
+    text=response or default_response
+  )
+
+
+if __name__ == "__main__":
+  if slack_client.rtm_connect(with_team_state=False):
+    print("Starter Bot connected and running!")
+    # Read bot's user ID by calling Web API method `auth.test`
+    starterbot_id = slack_client.api_call("auth.test")["user_id"]
+    while True:
+      command, channel = parse_bot_commands(slack_client.rtm_read())
+      if command:
+        handle_command(command, channel)
+      time.sleep(RTM_READ_DELAY)
+  else:
+    print("Connection failed. Exception traceback printed above.")
